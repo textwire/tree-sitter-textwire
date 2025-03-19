cd50de0ac48528e6e36a116c8a3870fb1e1d1a25
SerhiiCho <serhiicho@protonmail.com>
2025-03-19 16:00:06 +0200

    Change highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,3 @@
-; Keywords
-"@end" @keyword
-
 ; Variables
 (identifier) @variable
 
@@ -18,35 +15,19 @@
 (break_statement) @keyword
 (continue_statement) @keyword
 
-(dump_statement
-  "@dump" @keyword)
-
-(break_if_statement
-  "@breakIf" @keyword)
-
-(continue_if_statement
-  "@continueIf" @keyword)
-
-(if_statement
-  "@if" @keyword)
-
-(else_if_statement
-  "@elseif" @keyword)
-
-(else_statement
-  "@else" @keyword)
-
-(each_statement
-  ["@each" "in"] @keyword)
-
-(for_statement
-  "@for" @keyword)
-
-(component_statement
-  "@component" @keyword)
-
-(insert_statement
-  "@insert" @keyword)
-
-(reserve_statement
-  "@reserve" @keyword)
+; Keywords
+[
+  "@end"
+  "@dump"
+  "@breakIf"
+  "@continueIf"
+  "@if"
+  "@elseif"
+  "@else"
+  "@else"
+  "@each"
+  "@for"
+  "@component"
+  "@insert"
+  "@reserve"
+ ] @keyword
